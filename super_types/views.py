7695d0da4480002dca58ad218572b371f9d64688
kyle.macasinag <kylenmac@verizon.net>
2022-04-18 10:12:12 -0400

    Registered models with admin site

--- super_types/views.py
+++ super_types/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Supers
+from .serializers import SupersSerializer
+from rest_framework import status
+
+# from supers import serializers 
 
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def supers_list(request):
+    if request.method == 'GET':
+        supers = Supers.objects.all()
+        serializer = SupersSerializer(supers, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SupersSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def super_detail(request,pk):
+    supers = get_object_or_404(Supers, pk=pk)
+    if request.method =="GET":
+        serializer = SupersSerializer(supers)
+        return Response(serializer.data)
+    elif request.method =="PUT":
+        serializer = SupersSerializer(supers)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        supers.delete()
+        return Response(satus=status.HTTP_204_NO_CONTENT)
---
+++ supers/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Supers
+
+class SupersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Supers
+        fields = ['name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type']
+        depth = 1
+
+    super_type = serializers.IntegerField(write_only=True)
